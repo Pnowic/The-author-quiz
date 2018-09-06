ef9f8fd636456c2fe1071c38219e4eb7eee0d695
Pnowic <nowickipiotrus@gmail.com>
2018-09-06 18:58:48 +0200

    add images, complete non-interactive part

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
-class App extends Component {
-  render() {
+const Hero = (props) => {
     return (
-      <div>
-        The author quiz
-      </div>
+        <div className={"row"}>
+            <div className={"jumbotron col-10 offset-1"}>
+                <h1>The Author Quiz</h1>
+                <p>Select book written by the author shown</p>
+            </div>
+        </div>
     );
-  }
-}
+};
+
+const Book = ({title}) => {
+    return (<div className="answer">
+            <h4>{title}</h4>
+        </div>
+    );
+};
+
+const Turn = ({author, books}) => {
+    return (
+        <div className="row turn" style={{backgroundColor: "white"}}>
+            <div className="col-4 offset-1">
+                <img src={author.imageUrl} className="author-image" alt="Author" />
+            </div>
+            <div className="col-6">
+                {books.map((title) => <Book title={title} key={title} />)}
+            </div>
+        </div>
+    );
+};
+
+const Continue = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+const Footer = (props) => {
+    return (
+        <div id="footer" className="row">
+            <div className="col-12">
+                <p className="text-muted credit">All images are from
+                <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a></p>
+            </div>
+        </div>
+    );
+};
+
+
+const App = ({turnData}) => {
+    return (
+        <div className={"container-fluid"}>
+            <Hero />
+            <Turn {...turnData} />
+            <Continue />
+            <Footer />
+        </div>
+    );
+
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,70 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import shuffle, {sample} from 'underscore';
 
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.png',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining', 'IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charlesdickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/williamshakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+];
+
+const getTurnData = (authors) => {
+    const allBooks = authors.reduce((p, c, i) => {
+        return p.concat(c.books)
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    }
+};
+
+const state = {
+    turnData: getTurnData(authors)
+};
 
 function render (){
-    ReactDOM.render(<App/>, document.getElementById('root'));
+    ReactDOM.render(<App {...state}/>, document.getElementById('root'));
 }
 
 registerServiceWorker();
